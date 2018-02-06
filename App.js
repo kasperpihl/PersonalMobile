fe4e3b5eec118037050a59886f20dddcc41bfec1
thinklinux <tihomir.valkanov@gmail.com>
2018-02-06 10:50:38 +0100

    just the begining

--- App.js
+++ App.js
@@ -1,37 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{
+        width: '100%',
+        height: '100%',
+        borderWidth: 2,
+        borderColor: '#000000',
+      }}>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          width: '100%',
+          height: 50,
+          backgroundColor: 'transparent',
+          overflow: 'hidden',
+          
+        }}>
+
+          <View style={{
+            width: '100%',
+            height: 50,
+            backgroundColor: 'blue',
+            position: 'relative',
+            right: 100
+          }}>
+          </View>
+          <View style={{
+            width: '100%',
+            height: 50,
+            backgroundColor: 'red',
+            position: 'relative',
+          }}>
+          </View>
+
+        </View>
       </View>
     );
   }
