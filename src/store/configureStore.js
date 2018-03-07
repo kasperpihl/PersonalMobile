4ee3ef22b1bab1ece7b2706ef43f7d9fb83021b1
thinklinux <tihomir.valkanov@gmail.com>
2018-03-07 16:15:37 +0200

    deleting more stuff

--- src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-
-export default function configureStore(initialState) {
-	return createStore(
-		() => {{}},
-		initialState,
-		// applyMiddleware(...middleware)
-	);
-}
